5053f31a54aaab1d3fd00d714caffd502db7f725
Mattias Kindborg <FantasticFiasco@users.noreply.github.com>
2015-11-15 14:43:11 +0100

    Copied code from Laboration 1, but removed the data layer project.

---
+++ Laboration 2/Uppgift 1/MyMovies/Global.asax.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.Entity;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using MyMovies.Dal;
+
+namespace MyMovies
+{
+    public class MvcApplication : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            ConfigureDatabase();
+        }
+
+        private static void ConfigureDatabase()
+        {
+            Database.SetInitializer(new MoviesContextInitializer());
+        }
+    }
+}
