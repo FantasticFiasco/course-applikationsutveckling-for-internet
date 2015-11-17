5500bae4ca79b33239f45b96ed41dbf95bf5f4cd
Mattias Kindborg <FantasticFiasco@users.noreply.github.com>
2015-11-17 20:38:19 +0100

    Copied uppgift 1 to 3.

---
+++ Laboration 2/Uppgift 3/MyMovies/ViewModels/MovieViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace MyMovies.ViewModels
+{
+    public class MovieViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Display(Name = "Genre")]
+        public int SelectedGenreId { get; set; }
+
+        public SelectList Genre { get; set; }
+
+        [Range(1900, 2020)]
+        public int Year { get; set; }
+
+        [Range(0, 10)]
+        public double Rating { get; set; }
+
+        [Required]
+        public string Cast { get; set; }
+    }
+}
