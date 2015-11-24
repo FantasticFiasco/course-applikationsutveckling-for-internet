c9f7321438568b3b29a8361718afbd3a989c13d4
Mattias Kindborg <FantasticFiasco@users.noreply.github.com>
2015-11-24 22:58:06 +0100

    Added database connection string and configured the context initializer.

--- Laboration 3/Subscribers/Global.asax.cs
+++ Laboration 3/Subscribers/Global.asax.cs
@@ -1,5 +1,7 @@
-﻿using System.Web;
+﻿using System.Data.Entity;
+using System.Web;
 using System.Web.Http;
+using Subscribers.Models;
 
 namespace Subscribers
 {
@@ -7,6 +9,8 @@ namespace Subscribers
     {
         protected void Application_Start()
         {
+            Database.SetInitializer(new SubscriberContextInitializer());
+
             GlobalConfiguration.Configure(WebApiConfig.Register);
         }
     }
