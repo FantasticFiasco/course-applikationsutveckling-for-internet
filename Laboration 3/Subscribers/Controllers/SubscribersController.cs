217cd1826aed28f136f1c8336d1cb103e0cd1e88
Mattias Kindborg <FantasticFiasco@users.noreply.github.com>
2015-11-24 23:19:14 +0100

    Since the subscriber web API should be read-only I removed all methods in the controller that in some way altered the entities.

--- Laboration 3/Subscribers/Controllers/SubscribersController.cs
+++ Laboration 3/Subscribers/Controllers/SubscribersController.cs
@@ -1,7 +1,4 @@
-﻿using System.Data.Entity;
-using System.Data.Entity.Infrastructure;
-using System.Linq;
-using System.Net;
+﻿using System.Linq;
 using System.Web.Http;
 using System.Web.Http.Description;
 using Subscribers.Models;
@@ -36,70 +33,6 @@ namespace Subscribers.Controllers
             return Ok(subscriber);
         }
 
-        // PUT: api/Subscribers/5
-        [ResponseType(typeof(void))]
-        public IHttpActionResult PutSubscriber(int id, Subscriber subscriber)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            if (id != subscriber.Id)
-            {
-                return BadRequest();
-            }
-
-            context.Entry(subscriber).State = EntityState.Modified;
-
-            try
-            {
-                context.SaveChanges();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!SubscriberExists(id))
-                {
-                    return NotFound();
-                }
-                
-                throw;
-            }
-
-            return StatusCode(HttpStatusCode.NoContent);
-        }
-
-        // POST: api/Subscribers
-        [ResponseType(typeof(Subscriber))]
-        public IHttpActionResult PostSubscriber(Subscriber subscriber)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            context.Subscribers.Add(subscriber);
-            context.SaveChanges();
-
-            return CreatedAtRoute("DefaultApi", new { id = subscriber.Id }, subscriber);
-        }
-
-        // DELETE: api/Subscribers/5
-        [ResponseType(typeof(Subscriber))]
-        public IHttpActionResult DeleteSubscriber(int id)
-        {
-            Subscriber subscriber = context.Subscribers.Find(id);
-            if (subscriber == null)
-            {
-                return NotFound();
-            }
-
-            context.Subscribers.Remove(subscriber);
-            context.SaveChanges();
-
-            return Ok(subscriber);
-        }
-
         protected override void Dispose(bool disposing)
         {
             if (disposing)
@@ -107,11 +40,6 @@ namespace Subscribers.Controllers
                 context.Dispose();
             }
             base.Dispose(disposing);
-        }
-
-        private bool SubscriberExists(int id)
-        {
-            return context.Subscribers.Any(subscriber => subscriber.Id == id);
         }
     }
 }
