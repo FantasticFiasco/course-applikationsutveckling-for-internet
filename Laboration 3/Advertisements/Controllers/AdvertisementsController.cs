026343de9ca81d17b86a0470327b63399f0989fb
Mattias Kindborg <FantasticFiasco@users.noreply.github.com>
2016-01-02 10:21:19 +0100

    Added the possibility to add a subscriber advertisment.

--- Laboration 3/Advertisements/Controllers/AdvertisementsController.cs
+++ Laboration 3/Advertisements/Controllers/AdvertisementsController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Data.Entity;
 using System.Net;
 using System.Threading.Tasks;
 using System.Web.Mvc;
+using Advertisements.Models;
 using Advertisements.ViewModels;
 using Advertisements.WebServices;
 using Advertisements.WebServices.Requests;
@@ -11,10 +13,12 @@ namespace Advertisements.Controllers
 {
     public class AdvertisementsController : AsyncController
     {
+        private readonly AdvertisementContext context;
         private readonly SubscriberService subscriberService;
 
         public AdvertisementsController()
         {
+            context = new AdvertisementContext();
             subscriberService = new SubscriberService();
         }
 
@@ -56,7 +60,22 @@ namespace Advertisements.Controllers
         [HttpPost]
         public async Task<ActionResult> AddSubscriberAdvertisement(CreateAdvertisementViewModel viewModel)
         {
-            throw new NotImplementedException();
+            if (ModelState.IsValid)
+            {
+                Subscriber subscriber = await context.Subscribers.SingleOrDefaultAsync(s => s.SubscriptionNumber == viewModel.SubscriptionNumber);
+                if (subscriber == null)
+                {
+                    subscriber = CreateSubscriber(viewModel);
+                    context.Subscribers.Add(subscriber);
+                }
+
+                Advertisement advertisement = CreateAdvertisement(viewModel);
+                subscriber.Advertisements.Add(advertisement);
+
+                await context.SaveChangesAsync();
+            }
+
+            return View("Create", viewModel);
         }
 
         // POST: Advertisements/AddCompanyAdvertisement
@@ -77,6 +96,31 @@ namespace Advertisements.Controllers
                 Street = subscriber.Address.Street,
                 PostalCode = subscriber.Address.PostalCode,
                 City = subscriber.Address.City
+            };
+        }
+
+        private static Subscriber CreateSubscriber(CreateAdvertisementViewModel viewModel)
+        {
+            return new Subscriber
+            {
+                SubscriptionNumber = viewModel.SubscriptionNumber,
+                FirstName = viewModel.FirstName,
+                Surname = viewModel.Surname,
+                PhoneNumber = viewModel.PhoneNumber,
+                Street = viewModel.Street,
+                PostalCode = viewModel.PostalCode.Value,
+                City = viewModel.City
+            };
+        }
+
+        private static Advertisement CreateAdvertisement(CreateAdvertisementViewModel viewModel)
+        {
+            return new Advertisement
+            {
+                Title = viewModel.AdvertisementTitle,
+                Content = viewModel.AdvertisementContent,
+                Price = viewModel.AdvertisementPrice.Value,
+                AdvertisementCost = viewModel.SubscriptionNumber != null ? 0 : 40
             };
         }
 
--- Laboration 3/Advertisements/ViewModels/CreateAdvertisementViewModel.cs
+++ Laboration 3/Advertisements/ViewModels/CreateAdvertisementViewModel.cs
@@ -1,14 +1,18 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Advertisements.ViewModels
 {
-    public class CreateAdvertisementViewModel
+    public class CreateAdvertisementViewModel : IValidatableObject
     {
+        [Required]
         public string Street { get; set; }
 
+        [Required]
         [Display(Name = "Postal Code")]
-        public int PostalCode { get; set; }
+        public int? PostalCode { get; set; }
 
+        [Required]
         public string City { get; set; }
 
         #region Subscriber Members
@@ -36,7 +40,7 @@ namespace Advertisements.ViewModels
         public string InvoiceStreet { get; set; }
 
         [Display(Name = "Invoice Postal Code")]
-        public int InvoicePostalCode { get; set; }
+        public int? InvoicePostalCode { get; set; }
 
         [Display(Name = "Invoice City")]
         public string InvoiceCity { get; set; }
@@ -45,15 +49,59 @@ namespace Advertisements.ViewModels
 
         #region Advertisment
 
+        [Required]
         [Display(Name = "Advertisement Title")]
         public string AdvertisementTitle { get; set; }
 
+        [Required]
         [Display(Name = "Advertisement Content")]
         [DataType(DataType.MultilineText)]
         public string AdvertisementContent { get; set; }
 
+        [Required]
         [Display(Name = "Advertisement Price")]
-        public double AdvertisementPrice { get; set; }
+        public int? AdvertisementPrice { get; set; }
+
+        #endregion
+
+        #region IValidatableObject Members
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            return SubscriptionNumber != null ?
+                ValidateSubscriberAdvertisement() :
+                ValidateCompanyAdvertisement();
+        }
+
+        private IEnumerable<ValidationResult> ValidateSubscriberAdvertisement()
+        {
+            if (string.IsNullOrWhiteSpace(FirstName))
+                yield return new ValidationResult("The First Name field is required.", new[] { "FirstName" });
+
+            if (string.IsNullOrWhiteSpace(Surname))
+                yield return new ValidationResult("The Surname field is required.", new[] { "Surname" });
+
+            if (string.IsNullOrWhiteSpace(PhoneNumber))
+                yield return new ValidationResult("The Phone Number field is required.", new[] { "PhoneNumber" });
+        }
+
+        private IEnumerable<ValidationResult> ValidateCompanyAdvertisement()
+        {
+            if (string.IsNullOrWhiteSpace(Name))
+                yield return new ValidationResult("The Name field is required.", new[] { "Name" });
+
+            if (string.IsNullOrWhiteSpace(OrganisationNumber))
+                yield return new ValidationResult("The Organisation Number field is required.", new[] { "OrganisationNumber" });
+
+            if (string.IsNullOrWhiteSpace(InvoiceStreet))
+                yield return new ValidationResult("The Invoice Street field is required.", new[] { "InvoiceStreet" });
+
+            if (InvoicePostalCode == null)
+                yield return new ValidationResult("The Invoice Postal Code field is required.", new[] { "InvoicePostalCode" });
+
+            if (string.IsNullOrWhiteSpace(InvoiceCity))
+                yield return new ValidationResult("The Invoice City field is required.", new[] { "InvoiceCity" });
+        }
 
         #endregion
     }
