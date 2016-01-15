3e47a84aab471e71a4f1ad632a563bb5e259cd64
Mattias Kindborg <FantasticFiasco@users.noreply.github.com>
2016-01-15 08:24:09 +0100

    Fixed problem where forms for both subscriber and company was shown when page was loaded.

--- Laboration 3/Advertisements/Scripts/advertisements.js
+++ Laboration 3/Advertisements/Scripts/advertisements.js
@@ -1,18 +1,26 @@
 ﻿$(document).ready(function () {
 
-    // Enable subscriber controls when radio button is selected
-    $("#subscriberradio").change(function() {
+    function showSubscriber() {
         $("#subscriptionnumbertext").prop("disabled", false);
         $("#subscriptionnumberbutton").prop("disabled", false);
         $("#subscriberinput").show();
         $("#companyinput").hide();
-    });
+    }
 
-    // Disable subscriber controls when radio button is unselected
-    $("#companyradio").change(function() {
+    function showCompany() {
         $("#subscriptionnumbertext").prop("disabled", true);
         $("#subscriptionnumberbutton").prop("disabled", true);
         $("#subscriberinput").hide();
         $("#companyinput").show();
-    });
+    }
+
+    // Enable subscriber controls when radio button is selected
+    $("#subscriberradio").change(showSubscriber);
+
+    // Disable subscriber controls when radio button is unselected
+    $("#companyradio").change(showCompany);
+
+    if ($("#subscriberradio").is(":visible") && $("#companyradio").is(":visible")) {
+        showSubscriber();
+    }
 });
