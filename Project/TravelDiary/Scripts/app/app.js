c2c8971ce0b6e4290848446c059e88582d6dc9b9
Mattias Kindborg <FantasticFiasco@users.noreply.github.com>
2016-02-20 10:58:15 +0100

    Implemented showing the correct destination on the map when browsing diary entries.

--- Project/TravelDiary/Scripts/app/app.js
+++ Project/TravelDiary/Scripts/app/app.js
@@ -14,19 +14,49 @@
         return instance;
     })
 
-    .controller("DiaryEntryController", function ($scope, diaryEntryService) {
-
-        diaryEntryService.getLatestEntry()
-            .success(function (data) {
-                $scope.entry = data;
-            });
+    .controller("DiaryEntryController", function ($scope, diaryEntryService, NgMap) {
 
         $scope.getEntry = function (id) {
             diaryEntryService.getEntry(id)
                 .success(function (data) {
                     $scope.entry = data;
+                    $scope.updateMap();
                 });
         };
+
+        $scope.updateMap = function () {
+            // Remove current marker
+            if ($scope.marker != null) {
+                $scope.marker.setMap(null);
+            }
+
+            // Add new marker
+            var latLng = {
+                lat: $scope.entry.destination.latitude,
+                lng: $scope.entry.destination.longitude
+            };
+
+            $scope.map.setZoom(7);
+            $scope.map.setCenter(latLng);
+
+            $scope.marker = new google.maps.Marker({
+                position: latLng,
+                map: $scope.map,
+                title: $scope.entry.destination.name
+            });
+        }
+
+        diaryEntryService.getLatestEntry()
+            .success(function (data) {
+                $scope.entry = data;
+            })
+            .then(function () {
+                return NgMap.getMap();
+            })
+            .then(function (map) {
+                $scope.map = map;
+                $scope.updateMap();
+            });
     })
 
     .directive("diaryEntryText", function () {
